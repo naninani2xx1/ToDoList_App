16635edcdaea1d936ee03b96916416044e453e9c
naninani2xx1 <nhchuong2001@gmail.com>
2023-06-08 16:57:46 +0700

    setup splash page, icons app

---
+++ to_do_list/lib/config/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF130160);
+
+const Color secondaryColor = Color(0xFFFCA34D);
+
+const Color textFormColor = Color(0xFF0D0140);
+
+const Color blurColor = Color(0xFF99ABC6);
---
+++ to_do_list/lib/export.dart
@@ -0,0 +1,4 @@
+export 'package:flutter_native_splash/flutter_native_splash.dart';
+export 'package:to_do_list/src/firebase_options.dart';
+export 'package:firebase_core/firebase_core.dart';
+export 'package:flutter/material.dart';
